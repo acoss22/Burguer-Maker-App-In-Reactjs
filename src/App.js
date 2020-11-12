851ee6ef5136d59d73ed541e229d222e422be788
acoss22 <anacseq@gmail.com>
2020-11-12 14:42:16 +0000

    created a functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    //return (
-    //   <div className="App">
-    //     <h1>Hi, I'am a react app!</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hi, I'am a react app!</h1>
+        <p>This is really working! </p>
+        <Person/>
+      </div>
+    );
 
-    return React.createElement('div', null, 'h1', 'Hi, I\'am a react app!');
+    // return React.createElement('div', null, 'H1', 'Hi, I\'am a react app!');
   }
 }
 
