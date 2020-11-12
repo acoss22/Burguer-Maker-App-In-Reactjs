1c4dd2d9f35a3697215820897176da369fcc0115
acoss22 <anacseq@gmail.com>
2020-11-12 14:30:05 +0000

    changed app to class and  extending component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'am a react app!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    //return (
+    //   <div className="App">
+    //     <h1>Hi, I'am a react app!</h1>
+    //   </div>
+    // );
+
+    return React.createElement('div', null, 'h1', 'Hi, I\'am a react app!');
+  }
 }
 
 export default App;
